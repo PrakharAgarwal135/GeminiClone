a2b92fc8e0a786af06f59375ce26a7d735a8ccde
PrakharAgarwal135 <prakharagarwal135@gmail.com>
2024-12-14 00:03:37 +0530

    final

--- README.md
+++ README.md
@@ -13,11 +13,11 @@
 
 <h3>Technologies Used:</h3>
 
--  **HTML5**: Structured the content web page.
--  **CSS3**: Styling for layout and design of web page.
--  **JavaScript**: Added interactivity and handled API requests.
--  **Gemini API**: Integrated for real-time chatbot interaction.
--  **Axios**: Used for making HTTP requests to integrate the Gemini API for real-time chatbot functionality.
+- **HTML5**: Structured the content web page.
+- **CSS3**: Styling for layout and design of web page.
+- **JavaScript**: Added interactivity and handled API requests.
+- **Gemini API**: Integrated for real-time chatbot interaction.
+- **Axios**: Used for making HTTP requests to integrate the Gemini API for real-time chatbot functionality.
 
 ---
 
@@ -40,11 +40,3 @@
     <h5>Mobile View 2</h5>
     <img src="https://github.com/PrakharAgarwal135/GeminiClone/blob/main/images/readme%20ss/mob2.png" alt="Mobile View 2">
 </div>
-
-
-
-
----
-
-<h3>Setup:</h3>
-<p>To run this on your system locally replace the "your_gemini_api_key" in line 10 of app.js with your Gemini API key</p>
--- app.js
+++ app.js
@@ -1,95 +1,88 @@
 const typingForm = document.querySelector(".typing-form");
 const typingInput = document.querySelector(".typing-input");
 const chatList = document.querySelector(".chat-list");
-const suggestions = document.querySelectorAll(".suggestion-list .suggestion")
+const suggestions = document.querySelectorAll(".suggestion-list .suggestion");
 const sendButton = document.querySelector(".send-btn");
 
-let userMessage = null
-let isResponseGenerating = false
+let userMessage = null;
+let isResponseGenerating = false;
 
-const API_KEY= "your_gemini_api_key" ;
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`
+const API_KEY = "AIzaSyATDksjTtepCzPiXXLeXT1zkx8Z7sJzTMY";
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
-
-const createMessageElement = (content , ...className)=>{
-    // the ... (spread) adds all the passed classes 
-    const messageElement = document.createElement("div");
-    messageElement.classList.add("message" , ...className);
-    messageElement.innerHTML = content;
-    return messageElement;
-}
-
-
-const showTypingEffect = (text,textElement) =>{
-
-    // Split text into words
-    const words = text.split(' ');
-    let currentWordIndex = 0;
-
-    const typingInterval = setInterval(()=>{
-
-        // Append the next word with a leading space if it's not the first word
-        textElement.innerText+= (currentWordIndex == 0 ? '' : ' ') + words[currentWordIndex++]
-
-        // if all words are displayed 
-        if(currentWordIndex == words.length){
-            clearInterval(typingInterval);
-            // setting isResponseGenerating to false when response typed 
-            isResponseGenerating = false;
-        }
-        chatList.scrollTo(0,chatList.scrollHeight) //scroll to bottom
-    },75)
-}
-
-// fetching response from api 
-const generateApiResponse = async (incomingMessageDiv)=>{
-
-    const textElement = incomingMessageDiv.querySelector(".text");
-
-    // sending a post req to api with the user's message 
-    try {
-        const response = await axios.post(API_URL, 
-            JSON.stringify({
-                contents: [{
-                    role: "user",
-                    parts: [{ text: userMessage }]
-                }]
-            }),
-            {
-                headers: {'Content-Type': 'application/json'}
-            }
-        );
-
-        
-        // Adjusting path based on the API response structure
-        const apiResponse = response.data.candidates[0].content.parts[0].text.replace(/\*/g, '');
-        // console.log(apiResponse)
-
-        // textElement.innerText = apiResponse;
-
-        showTypingEffect(apiResponse,textElement)
-        
-        
-    } catch (error) {
-        // setting isResponseGenerating to false if an error occurs 
-        isResponseGenerating = false
-        
-        textElement.innerText =error.message;
-        textElement.classList.add('error')
-
-        console.log(error)
-    }
-    finally{
-        incomingMessageDiv.classList.remove("loading")
-    }
+const createMessageElement = (content, ...className) => {
+  // the ... (spread) adds all the passed classes
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("message", ...className);
+  messageElement.innerHTML = content;
+  return messageElement;
 };
 
+const showTypingEffect = (text, textElement) => {
+  // Split text into words
+  const words = text.split(" ");
+  let currentWordIndex = 0;
 
+  const typingInterval = setInterval(() => {
+    // Append the next word with a leading space if it's not the first word
+    textElement.innerText +=
+      (currentWordIndex == 0 ? "" : " ") + words[currentWordIndex++];
+
+    // if all words are displayed
+    if (currentWordIndex == words.length) {
+      clearInterval(typingInterval);
+      // setting isResponseGenerating to false when response typed
+      isResponseGenerating = false;
+    }
+    chatList.scrollTo(0, chatList.scrollHeight); //scroll to bottom
+  }, 75);
+};
+
+// fetching response from api
+const generateApiResponse = async (incomingMessageDiv) => {
+  const textElement = incomingMessageDiv.querySelector(".text");
+
+  // sending a post req to api with the user's message
+  try {
+    const response = await axios.post(
+      API_URL,
+      JSON.stringify({
+        contents: [
+          {
+            role: "user",
+            parts: [{ text: userMessage }],
+          },
+        ],
+      }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+
+    // Adjusting path based on the API response structure
+    const apiResponse =
+      response.data.candidates[0].content.parts[0].text.replace(/\*/g, "");
+    // console.log(apiResponse)
+
+    // textElement.innerText = apiResponse;
+
+    showTypingEffect(apiResponse, textElement);
+  } catch (error) {
+    // setting isResponseGenerating to false if an error occurs
+    isResponseGenerating = false;
+
+    textElement.innerText = error.message;
+    textElement.classList.add("error");
+
+    console.log(error);
+  } finally {
+    incomingMessageDiv.classList.remove("loading");
+  }
+};
 
 // handle loading animations and adding them to chatList
-const showLoadingAnimation = ()=>{
-    const html = 
-    `<div class="message-content">
+const showLoadingAnimation = () => {
+  const html = `<div class="message-content">
         <img src="images/gemini.svg" alt="Gemini Img" class="avatar">
         <p class="text"> </p>
         <div class="loading-indicator">
@@ -98,76 +91,68 @@ const showLoadingAnimation = ()=>{
             <div class="loading-bar"></div>
         </div>
     </div>
-    <span onclick="copyMessage(this)" class="icon material-symbols-rounded">content_copy</span>`
+    <span onclick="copyMessage(this)" class="icon material-symbols-rounded">content_copy</span>`;
 
-    const incomingMessageDiv = createMessageElement(html,"incoming" , "loading")
-    chatList.appendChild(incomingMessageDiv);
+  const incomingMessageDiv = createMessageElement(html, "incoming", "loading");
+  chatList.appendChild(incomingMessageDiv);
 
-    chatList.scrollTo(0,chatList.scrollHeight) //scroll to bottom
+  chatList.scrollTo(0, chatList.scrollHeight); //scroll to bottom
 
-    generateApiResponse(incomingMessageDiv);
-}
+  generateApiResponse(incomingMessageDiv);
+};
 
+// copy messages to clipboard using copy icon
+const copyMessage = (copyIcon) => {
+  const messageText = copyIcon.parentNode.querySelector(".text").innerText;
+  navigator.clipboard.writeText(messageText);
+  copyIcon.innerText = "done"; //show tick icon
+  setTimeout(() => (copyIcon.innerText = "content_copy"), 1000); //revert icon after 1 second
+};
 
-// copy messages to clipboard using copy icon 
-const copyMessage=(copyIcon) =>{
-    const messageText = copyIcon.parentNode.querySelector('.text').innerText;
-    navigator.clipboard.writeText(messageText)
-    copyIcon.innerText ="done"; //show tick icon
-    setTimeout(()=>copyIcon.innerText="content_copy",1000); //revert icon after 1 second
-}
+// handle sending outgoing chat messages and adding them to our chatList
+const handleOutgoingChat = () => {
+  // trim remove extra white spaces from both sides
+  userMessage = typingInput.value.trim() || userMessage;
 
+  // exit if no message or a response is being generated
+  if (!userMessage || isResponseGenerating) return;
 
-// handle sending outgoing chat messages and adding them to our chatList 
-const handleOutgoingChat = () =>{
-    // trim remove extra white spaces from both sides 
-    userMessage = typingInput.value.trim() || userMessage;
-  
-    // exit if no message or a response is being generated
-    if(!userMessage || isResponseGenerating) return;
+  // now users cant send another message when the current one is being generated
+  isResponseGenerating = true;
 
-    // now users cant send another message when the current one is being generated 
-    isResponseGenerating =true
+  // console.log(userMessage)
 
-    // console.log(userMessage)
-
-
-    const html = 
-    `<div class="message-content">
+  const html = `<div class="message-content">
         <img src="images/user.jpg" alt="User Img" class="avatar">
         <p class="text"></p>
-    </div>`
+    </div>`;
 
-    const outgoingMessageDiv = createMessageElement(html , "outgoing");
-    outgoingMessageDiv.querySelector(".text").innerText = userMessage;
-    chatList.appendChild(outgoingMessageDiv);
+  const outgoingMessageDiv = createMessageElement(html, "outgoing");
+  outgoingMessageDiv.querySelector(".text").innerText = userMessage;
+  chatList.appendChild(outgoingMessageDiv);
 
+  // clear input field
+  typingForm.reset();
 
-    // clear input field 
-    typingForm.reset();
+  chatList.scrollTo(0, chatList.scrollHeight); //scroll to bottom
 
-    chatList.scrollTo(0,chatList.scrollHeight) //scroll to bottom
+  // hide the header once chat start
+  document.body.classList.add("hide-header");
 
-    // hide the header once chat start 
-    document.body.classList.add("hide-header")
+  // show loading animation after delay
+  setTimeout(showLoadingAnimation, 500);
+};
 
-    // show loading animation after delay 
-    setTimeout(showLoadingAnimation,500);
-
-}
-
-
-
-// set userMessage and handle outgoing chat when clicked on suggestion 
-suggestions.forEach((suggestion)=>{
-    suggestion.addEventListener('click',()=>{
-        userMessage = suggestion.querySelector('.text').innerText
-        handleOutgoingChat()
-    })
-})
+// set userMessage and handle outgoing chat when clicked on suggestion
+suggestions.forEach((suggestion) => {
+  suggestion.addEventListener("click", () => {
+    userMessage = suggestion.querySelector(".text").innerText;
+    handleOutgoingChat();
+  });
+});
 
 // Prevent default form submission and handle outgoing chat
-typingForm.addEventListener("submit",(e)=>{
-    e.preventDefault();
-    handleOutgoingChat();
-})
+typingForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  handleOutgoingChat();
+});
